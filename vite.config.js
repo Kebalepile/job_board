b4e2cb3aa3c01f2d895c470ad94bddcb0cfabe20
Kebalepile <pimpabutterfly86@gmail.com>
2024-07-19 09:29:18 +0200

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-// vite.config.js
 export default {
   build: {
+    chunkSizeWarningLimit: 500, // Set the limit to 500 kB
+
     rollupOptions: {
       output: {
         manualChunks(id) {
           // Specify manual chunks based on your code structure
-          if (id.includes('node_modules')) {
-            return 'vendor';
+          if (id.includes("node_modules")) {
+            return "vendor";
           }
-        },
-      },
-    },
-  },
+        }
+      }
+    }
+  }
 };
-// vite.config.js
-// export default {
-//   build: {
-//     chunkSizeWarningLimit: 1024 * 500, // Set the limit to 500 kB
-//   },
-// };
