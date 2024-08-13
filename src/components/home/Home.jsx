fd806cc89c29a719563632180cfc5b16018a6862
Kebalepile <pimpabutterfly86@gmail.com>
2024-08-13 14:54:17 +0200

    fixing project structure

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,54 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import useLoadingPlaceholder from '../../hooks/useLoadingPlaceholder'
-import { useNavigate } from 'react-router-dom'
-import './home.css'
-import agencyData from '../../assets/agencyData.json' // Import the JSON file
-import About from '../about/About'
-import Footer from '../footer/Footer'
+import React, { useState, useEffect } from 'react';
+import useLoadingPlaceholder from '../../hooks/useLoadingPlaceholder';
+import { useNavigate } from 'react-router-dom';
+import './home.css';
+import imageUrls from '../../assets/agency_icons/imageUrls.json'; 
+import About from '../about/About';
+import Footer from '../footer/Footer';
 
-export default function Home () {
-  const [currentIndex, setCurrentIndex] = useState(0)
-  const [isLoaded] = useLoadingPlaceholder(1000)
-  const [isSliderContainerLoaded] = useLoadingPlaceholder(1000)
+export default function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [isLoaded] = useLoadingPlaceholder(1000);
+  const [isSliderContainerLoaded] = useLoadingPlaceholder(1000);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  // Get image URLs from the JSON data
+  const images = imageUrls.imageUrls;
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentIndex(prevIndex => (prevIndex + 1) % agencyData.length)
-    }, 5000)
+      setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
+    }, 5000);
 
-    return () => clearInterval(interval)
-  }, [])
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   const goToSlide = index => {
-    setCurrentIndex(index)
-  }
+    setCurrentIndex(index);
+  };
 
   const navigateToVacancies = () => {
-    navigate('/vacancies')
-  }
+    navigate('/vacancies');
+  };
 
   return (
     <>
       {isSliderContainerLoaded ? (
         <div className='slider-container'>
-          {isLoaded && agencyData.length > 0 ? (
+          {isLoaded && images.length > 0 ? (
             <>
               <img
-                src={agencyData[currentIndex].image}
+                src={images[currentIndex]}
                 alt='Agency Icon'
                 className='slide-image'
               />
               <p className='slider-message'>
-                {agencyData[currentIndex].message}
+                {/* Optionally, add a message or caption */}
               </p>
             </>
           ) : (
             <div className='placeholder loading-placeholder'></div>
           )}
           <div className='dots-container'>
-            {agencyData.map((_, index) => (
+            {images.map((_, index) => (
               <span
                 key={index}
                 className={`dot ${index === currentIndex ? 'active' : ''}`}
@@ -67,5 +70,5 @@ export default function Home () {
       <About />
       <Footer />
     </>
-  )
+  );
 }
--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PDFViewerIframe from '../pdf/PDFViewerIframe'
-import { getPdfTitle } from '../../utils/getPdfTitle'
+import pdfUrls from '../../assets/pdfs/pdfUrls.json'
 import './home.css'
 
 export default function HomePage () {
-  const pdfFiles = [
-    {
-      file: '/assets/pdfs/PANEL-OF-EXECUTIVE-MANAGING-AGENTS-ADVERT-FINAL.pdf'
-    },
-    {
-      file: "/assets/pdfs/ARMC_Members_x2(Aug'24).pdf"
-    },
-    {
-      file: '/assets/pdfs/Advert_Junior_Compliance_Analyst_Aug_2024.pdf'
-    }
-  ]
+  const pdfFiles = pdfUrls.pdfUrls
 
   return (
     <div>
       <h1>Vacancies</h1>
       <div className='pdf-container'>
-        {pdfFiles.map((pdf, index) => (
+        {pdfFiles.map((url, index) => (
           <div key={index} className='pdf-viewer-iframe'>
-            <PDFViewerIframe file={pdf.file} />
+            <PDFViewerIframe file={url} />
           </div>
         ))}
       </div>
